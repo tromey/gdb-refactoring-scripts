c34eba6704bf240001572c25e32cb706cc7002b4
Tom Tromey <tom@tromey.com>
2019-01-21 19:38:56 -0700

    handle conditional include blocks

--- rewriter-includes.el
+++ rewriter-includes.el
@@ -1,5 +1,9 @@
 ;; Sort includes.
 
+;; FIXME rewrite to common/
+;; FIXME gdbserver/ax.c -vs- ax.h
+;; FIXME insert header guards
+
 (defun rw-skip-intro (filename is-header)
   ;; Skip all available.
   (forward-comment 9999)
@@ -12,26 +16,60 @@
       (message "%s does not have #define !?" filename))
     (skip-chars-forward " \t\r\n")))
 
-;; FIXME rewrite to common/
-
-;; FIXME leave point at line after last include
-(defun rw-collect-includes ()
-  (let ((result '())
-	(last-include (point)))
-    (while (progn
-	     (skip-chars-forward " \t\r\n")
-	     (looking-at "#include \\([\"<]\\)\\([^\">]*\\)[\">]"))
+(defun rw-scan-condition ()
+  (let ((kind nil)
+	(start (point))
+	(ok t)
+	(first-name nil))
+    ;; Looking at the #if.
+    (forward-line)
+    (while (looking-at "#include \\([\"<]\\)\\([^\">]*\\)[\">]")
       (let ((style (match-string 1))
 	    (name (match-string 2)))
-	;; This eventually and trickily arranges for non-gdb headers
-	;; to be included earlier.
-	(when (and (string= style "\"")
-		   (not (member (expand-file-name name rw-directory) (rw-files))))
-	  ;; Must be between " and <.
-	  (setq style "."))
-	(push (list name style) result))
-      (forward-line)
-      (setq last-include (point)))
+	(unless first-name
+	  (setq first-name name))
+	(if kind
+	    (unless (equal kind style)
+	      (message "Inconsistent conditional include at %s"
+		       (line-number-at-pos))
+	      (setq ok nil))
+	  (setq kind style)))
+      (forward-line))
+    (if (and ok (looking-at "#endif"))
+	(progn
+	  (forward-line)
+	  (list first-name kind (buffer-substring start (1- (point)))))
+      (goto-char start)
+      nil)))
+
+(defun rw-collect-includes ()
+  (let ((result '())
+	(last-include (point))
+	(keep-going t))
+    (while keep-going
+      (setq keep-going nil)
+      (skip-chars-forward " \t\r\n")
+      (cond
+       ((looking-at "#include \\([\"<]\\)\\([^\">]*\\)[\">]")
+	(let ((style (match-string 1))
+	      (name (match-string 2)))
+	  ;; This eventually and trickily arranges for non-gdb headers
+	  ;; to be included earlier.
+	  (when (and (string= style "\"")
+		     (not (member (expand-file-name name rw-directory)
+				  (rw-files))))
+	    ;; Must be between " and <.
+	    (setq style "."))
+	  (push (list name style (match-string 0)) result))
+	(forward-line)
+	(setq last-include (point))
+	(setq keep-going t))
+       ((looking-at "#if.*HAVE_[A-Z0-9_]*_H")
+	(let ((cond-include (rw-scan-condition)))
+	  (when cond-include
+	    (push cond-include result)
+	    (setq last-include (point))
+	    (setq keep-going t))))))
     (goto-char last-include)
     result))
 
@@ -85,15 +123,7 @@
 	(unless (string= last-bracket new-bracket)
 	  (insert "\n")
 	  (setq last-bracket new-bracket))
-	(insert "#include "
-		(if (string= new-bracket "<")
-		    "<"
-		  "\"")
-		(car item)
-		(if (string= new-bracket "<")
-		    ">"
-		  "\"")
-		"\n")))))
+	(insert (nth 2 item) "\n")))))
 
 (defun rw-rewrite-includes ()
   (unless buffer-read-only
