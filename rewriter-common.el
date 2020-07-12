f883af6dfd4de45c12315a02ca545c3b293d83b1
Tom Tromey <tom@tromey.com>
2020-07-12 12:17:24 -0600

    handle gdbsupport move

--- rewriter-common.el
+++ rewriter-common.el
@@ -1,17 +1,16 @@
 ;; Rewrite include x to include common/x
 
-(defconst rw-common-dir (expand-file-name "common" rw-directory))
+(defconst rw-common-dir (expand-file-name "gdbsupport" rw-base-directory))
 
 (defun rw-common-includes ()
   (goto-char (point-min))
   ;; could maybe check for "" -vs- <> consistency.
-  (while (re-search-forward "^#\\s-*include \"\\([^\"]*\\)\"" nil t)
+  ;; Note this avoids "/" in the name.
+  (while (re-search-forward "^#\\s-*include \"\\([^\"/]*\\)\"" nil t)
     (let ((name (match-string 1)))
-      (unless (or (file-exists-p (expand-file-name name))
-		  (file-exists-p (expand-file-name name rw-directory)))
-	(when (file-exists-p (expand-file-name name rw-common-dir))
-	  (replace-match (concat "common/" name) nil t nil 1)
-	  (rw-add-change-log-entry)))))
-  (rw-final-change-log-text "Fix common/ includes."))
+      (when (file-exists-p (expand-file-name name rw-common-dir))
+	(replace-match (concat "gdbsupport/" name) nil t nil 1)
+	(rw-add-change-log-entry))))
+  (rw-final-change-log-text "Fix gdbsupport/ includes."))
 
 (rw-rewrite #'rw-common-includes)
